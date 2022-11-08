a338c8a0976d9191e8e1e67823a3794863831e60
James Demiraiakian <james.armen@gmail.com>
2022-11-08 01:32:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Base!
+
+This engineer left the template readme in place, and did not document their
+project with a simple readme.
+
+## Testing
+
+This application uses Jest for testing the React side, and Supertest for server
+tests.
+
+## Deployment
+
+Coming soon!
---
+++ server/app.ts
@@ -0,0 +1,19 @@
+import express, { type Request, type Response } from 'express'
+
+const app = express()
+
+const server = app.listen(parseInt(process.env.APP_PORT || '7890'), () => {
+  console.log('Started server on ', server.address())
+})
+
+// Think of the poor foos.
+app.get('/foos', (req: Request, res: Response) => {
+  res.send([ { foo: 'bar' } ])
+})
+
+app.all('*', (req: Request, res: Response) => {
+  console.log(`404 for ${req.url}`)
+  res.status(404).send({ error: 404, message: `URL ${req.url} not found` })
+})
+
+export default server
